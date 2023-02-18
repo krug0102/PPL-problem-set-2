6cdcf01a827587f5e79e96f4ab735d9ad9d9b7af
krug0102 <krug0102@morris.umn.edu>
2023-02-17 21:19:16 -0600

    Finish impl macro Added tests for `fib`, `count-seqs`, and `impl` functions/macro.

--- src/problem_set_2/core.clj
+++ src/problem_set_2/core.clj
@@ -1,86 +1,105 @@
 (ns problem-set-2.core
-  (:gen-class))
+(:gen-class))
 
-  ;;; Problem 1 ;;;
-  ; (defn fib-no-loop
-  ;   "takes a number n, computes the nth fibonacci number"
-  ;   [n]
-  ;   (cond
-  ;     (= n 0) n
-  ;     (= n 1) n
-  ;     :else (+ (fib-no-loop (dec n)) (fib-no-loop (- n 2)))))
+;;; Problem 1 ;;;
+; (defn fib-no-loop
+;   "takes a number n, computes the nth fibonacci number"
+;   [n]
+;   (cond
+;     (= n 0) n
+;     (= n 1) n
+;     :else (+ (fib-no-loop (dec n)) (fib-no-loop (- n 2)))))
 
-  (defn fib
-    "takes a number n, computes the nth fibonacci number"
-    [n]
-    (if (< n 2)
-      n
-      (loop [i 1 lst 0 nxt 1]
-        (if (>= i n)
-          nxt
-          (recur (inc i) nxt (+ lst nxt))))))
+(defn fib
+  "takes a number n, computes the nth fibonacci number"
+  [n]
+  (if (< n 2)
+    n
+    (loop [i 1 lst 0 nxt 1]
+      (if (>= i n)
+        nxt
+        (recur (inc i) nxt (+ lst nxt))))))
 
 
-  ;;; Problem 2 ;;;
-  (def nested-vectors [[1] [2 [3]] [[[4 5] 6 [7 8]] [9 []] ]]) ; 10 nested sequences
+;;; Problem 2 ;;;
+(def nested-vectors [[1] [2 [3]] [[[4 5] 6 [7 8]] [9 []] ]]) ; 10 nested sequences
 
-  (defn count-seqs-recur
-    "helper function which does the actual recursion, and keeps track of the count"
-    [seq count] ; count will be initialized as 0
-    (cond
-      (not (seqable? seq)) count ; if seq is not a sequence, return 0
-      (and (empty? (rest seq)) (not (seqable? (first seq)))) (inc count) ; if seq is the only element and not a sequence, return 1
-      (seqable? (first seq)) (count-seqs-recur (first seq) (inc count))
-      :else (count-seqs-recur (rest seq) count))) ; otherwise, look at the first element in the sequences
-      ;;; TODO: how to do i move to the next element in seq?
+(defn count-seqs-recur
+  "helper function which does the actual recursion, and keeps track of the count"
+  [seq count] ; count will be initialized as 0
+  (cond
+    (not (seqable? seq)) count ; if seq is not a sequence, return 0
+    (and (empty? (rest seq)) (not (seqable? (first seq)))) (inc count) ; if seq is the only element and not a sequence, return 1
+    (seqable? (first seq)) (count-seqs-recur (first seq) (inc count))
+    :else (count-seqs-recur (rest seq) count))) ; otherwise, look at the first element in the sequences
+    ;;; TODO: how to do i move to the next element in seq?
 
-  (defn count-seqs
-    "takes a sequence that has nested sequences in it, returns the total number of nested sequences,
-    including the outer sequence"
-    [seq]
-    (count-seqs-recur seq 0))
+(defn count-seqs
+  "takes a sequence that has nested sequences in it, returns the total number of nested sequences,
+  including the outer sequence"
+  [seq]
+  (count-seqs-recur seq 0))
 
-  (= 0 (count-seqs 5))
-  (= 1 (count-seqs [1]))
-  (= 2 (count-seqs [1 [2]]))
-  (= 2 (count-seqs [[1] 2]))
-  (= 3 (count-seqs [1 [2 [3]]]))
-  (= 4 (count-seqs [[1] [2 [3]]]))
-  (= 10 (count-seqs nested-vectors))
 
-  ;;; Problem 3 ;;;
-  ;; 1. Sort a sequence of strings by length ;;
-  (defn compare-lengths
-    "comparator function that takes two strings, returns:
-     -1 if length of p is < length of q
-     0 if length of p = length of q
-     1 if length of p is > length q"
-    [p q]
-    (cond
-      (< (.length p) (.length q)) -1
-      (= (.length p) (.length q)) 0
-      (> (.length p) (.length q)) 1))
+;;; Problem 3 ;;;
+;; 1. Sort a sequence of strings by length ;;
+(defn compare-lengths
+  "comparator function that takes two strings, returns:
+   -1 if length of p is < length of q
+   0 if length of p = length of q
+   1 if length of p is > length q"
+  [p q]
+  (cond
+    (< (.length p) (.length q)) -1
+    (= (.length p) (.length q)) 0
+    (> (.length p) (.length q)) 1))
 
-  (defn sort-strings
-    "takes a sequence of strings and sorts them by length"
-    [seq]
-    (sort compare-lengths seq))
+(defn sort-strings
+  "takes a sequence of strings and sorts them by length"
+  [seq]
+  (sort compare-lengths seq))
 
-  ;; 2. Sort a sequence of hashmaps that have the keys :name, :age, :hometown by the :age field ;;
-  (defn compare-hashmaps
-    "takes two hashmaps, returns:
-    -1 if :age value of p is < :age value of q
-    0 if :age value of p is = :age value of q
-    1 if :age value of p is > :age value of q"
-    [m1 m2]
-    (cond
-      (< (:age m1) (:age m2)) -1
-      (= (:age m1) (:age m2)) 0
-      (> (:age m1) (:age m2)) 1))
+;; 2. Sort a sequence of hashmaps that have the keys :name, :age, :hometown by the :age field ;;
+(defn compare-hashmaps
+  "takes two hashmaps, returns:
+  -1 if :age value of p is < :age value of q
+  0 if :age value of p is = :age value of q
+  1 if :age value of p is > :age value of q"
+  [p q]
+  (cond
+    (< (:age p) (:age q)) -1
+    (= (:age p) (:age q)) 0
+    (> (:age p) (:age q)) 1))
 
-  (defn sort-ages
-    "takes a sequence of hashmaps with keys :name, :age, :hometown and sorts them by :age field"
-    [seq]
-    (sort compare-hashmaps seq))
+(defn sort-ages
+  "takes a sequence of hashmaps with keys :name, :age, :hometown and sorts them by :age field"
+  [seq]
+  (sort compare-hashmaps seq))
 
-  ;; 3. Sort a sequence of BigIntegers by their values
+;; 3. Sort a sequence of BigIntegers by their values
+(defn compare-BigIntegers
+  "comparator function that takes two strings, returns:
+   -1 if the int value of p is < the int value of q
+   0 if the int value of p = the int value of q
+   1 if the int value of p is > the int value q"
+  [p q]
+  (cond
+    (< (.intValue p) (.intValue q)) -1
+    (= (.intValue p) (.intValue q)) 0
+    (> (.intValue p) (.intValue q)) 1))
+
+(defn sort-BigIntegers
+  "takes a sequence of BigIntegers and sorts them by their values"
+  [seq]
+  (sort compare-BigIntegers seq))
+
+
+;;; Problem 4 ;;;
+(defmacro impl
+  "macro which represents logical implication"
+  [p q]
+  `(if (not ~p)
+    true
+    (if ~q
+      true
+      false)))
--- test/problem_set_2/core_test.clj
+++ test/problem_set_2/core_test.clj
@@ -2,6 +2,30 @@
   (:require [clojure.test :refer :all]
             [problem-set-2.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+
+(deftest fib-testing
+  (testing "fib function"
+    (is (= 0 (fib 0)))
+    (is (= 1 (fib 1)))
+    (is (= 1 (fib 2)))
+    (is (= 2 (fib 3)))
+    (is (= 55 (fib 10)))
+    (is (= 4181 (fib 19)))))
+
+(deftest count-seq-testing
+  (testing "count-seqs function"
+  (is (= 0 (count-seqs 5)))
+  (is (= 1 (count-seqs [1])))
+  (is (= 2 (count-seqs [1 [2]])))
+  (is (= 2 (count-seqs [[1] 2])))
+  (is (= 3 (count-seqs [1 [2 [3]]])))
+  (is (= 4 (count-seqs [[1] [2 [3]]])))
+  (is (= 10 (count-seqs nested-vectors)))))
+
+(deftest impl-testing
+  (testing "impl macro"
+    (is (true? (impl (< 2 3) (< 5 6))))
+    (is (true? (impl (> 2 3) (< 5 (/ 1 0)))))
+    (is (true? (impl (> 2 3) (< 5 6))))
+    (is (false? (impl (< 2 3) (> 5 6))))
+    (is (false? (impl (< 2 3) nil)))))
